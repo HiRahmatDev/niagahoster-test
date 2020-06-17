9716eed204eec3bd419e67c8c2aa26205786ef27
HiRahmat-Dev <ahmadalhidayah1@gmail.com>
2020-06-17 16:18:23 +0700

    Update

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/prices', 'PricesController@all');
 Route::get('/prices/{id}','PricesController@show');
 Route::post('/prices','PricesController@store');
